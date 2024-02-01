507a9ae94a795e80c17b013a8d9ebf4338803c71
githubscientist <sathish.datascientist@gmail.com>
2024-02-01 11:16:10 +0530

    useEffect without dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+/*
+  useEffect: 
+    - useEffect is a hook that allows you to perform side effects in function components
+    - side effects are anything that affects something outside of the scope of the function
+      - examples: 
+        - fetching data
+        - setting up a subscription
+        - manually changing the DOM
+    - we can use useEffect in two ways:
+      a. to run code only when the component mounts
+      b. to run code when the component mounts and when it updates
+*/
+
+// Show the list of users from the API, https://jsonplaceholder.typicode.com/users
 const App = () => {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json();
+
+        setUsers(users);
+        setLoading(false);
+
+        console.log(users);
+      } catch (error) {
+        console.log('error fetching users', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
-    <div>App</div>
+    <div>
+      {
+        loading ? (
+          <p>loading data...</p>
+        ) : (
+            <div>
+              <h2>List of Users</h2>
+
+              <ul>
+                {
+                  users.map(user => (
+                    <li key={user.id}>
+                      {user.name}
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+        )
+      }
+    </div>
   )
 }
 
