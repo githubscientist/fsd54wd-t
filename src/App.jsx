88c82f76055ab5ad680b86f9204ac0e7e4839c9d
githubscientist <sathish.datascientist@gmail.com>
2024-02-08 10:27:10 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,8 @@
-import { useState } from "react";
-import CreateTodoForm from "./components/CreateTodoForm";
-import TodosList from "./components/TodosList";
+import React from 'react';
 
 const App = () => {
-
-  const [todos, setTodos] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
-  const [newTodoItem, setNewTodoItem] = useState({
-    name: '',
-    description: '',
-    status: 'Not Completed'
-  });
-  const [newItemIndex, setNewItemIndex] = useState(null);
-
   return (
-    <div>
-      <h3>My Todo</h3>
-
-      <CreateTodoForm 
-        todos={todos}
-        setTodos={setTodos}
-        isEditing={isEditing}
-        setIsEditing={setIsEditing}
-        newTodoItem={newTodoItem}
-        setNewTodoItem={setNewTodoItem}
-        newItemIndex={newItemIndex}
-      />
-
-      <TodosList 
-        todos={todos}
-        setTodos={setTodos}
-        setIsEditing={setIsEditing}
-        newTodoItem={newTodoItem}
-        setNewTodoItem={setNewTodoItem}
-        setNewItemIndex={setNewItemIndex}
-      />
-    </div>
+    <div>App</div>
   )
 }
 
