386098479f35cdb1d94c3036601176438ce0a53e
githubscientist <sathish.datascientist@gmail.com>
2024-02-15 10:50:23 +0530

    a simple counter using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
+/*
+  useReducer hook is used to manage the state of the application.
+
+  - despite useState hook, useReducer hook is used when the state logic is complex and involves multiple sub-values or when the next state depends on the previous one.
+
+  - useReducer hook is similar to Redux, but it is built into React.
+
+  - Here, we will put the state logic in a separate file and import it into the component.
+*/
+
+import { useState } from "react";
+
 const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [clicksHistory, setClicksHistory] = useState([]);
+
+  const increment = () => {
+    setCount(count + 1);
+    setClicksHistory([...clicksHistory, 'Incr']);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+    setClicksHistory([...clicksHistory, 'Decr']);
+  }
+
+  const reset = () => {
+    setCount(0);
+    setClicksHistory([...clicksHistory, 'Reset']);
+  }
+
   return (
-    <div>App</div>
+    <>
+      <h3>Counter: { count }</h3>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+
+      <h3>Clicks History</h3>
+      <p>{ clicksHistory.join(', ') }</p>
+    </>
   )
 }
 
